21bdfc807ca99e550c220b01d91e8fa8107dca51
ral7ex <98921679+ral7ex@users.noreply.github.com>
2023-02-04 13:10:12 -0500

    Created initial files

---
+++ ChessGame.java
@@ -0,0 +1,18 @@
+
+public class ChessGame {
+
+	/* A class implementing the game of chess itself.
+	 * Takes moves from each player and runs the game.
+	 * */
+	
+	
+	//variables
+	
+	//something to represent the chess board
+	//game move log.
+	
+	public ChessGame() {
+		
+	}
+	
+}
