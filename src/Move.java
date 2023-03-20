261a66745d2f1c85d40284bc653b6c11547e31e9
ral7ex <98921679+ral7ex@users.noreply.github.com>
2023-03-19 21:45:29 -0400

    Implemented Move class conversions, constructors, and toString

--- src/Move.java
+++ src/Move.java
@@ -1,12 +1,18 @@
 
 public class Move {
-	int r1;
+	int r1; //board indexes
 	int r2;
 	int c1;
 	int c2;
 	public Move (String s) throws IllegalArgumentException {
+		s = s.toLowerCase();
 		if (s.length() != 4) throw new IllegalArgumentException();
 		//convert string codes to it
+		r1 = intToInt(s.charAt(1)); // -1 because index
+		r2 = intToInt(s.charAt(3));
+		c1 = letterToInt(s.charAt(0));
+		c2 = letterToInt(s.charAt(2));
+		if (c1==-1 || c2==-1 || r1 ==-1 || r2 ==-1) throw new IllegalArgumentException();
 		
 		
 		
@@ -18,5 +24,57 @@ public class Move {
 		r2 =r_dest;
 		c1 = c;
 		c2 = c_dest;
+	}
+	
+	public String toString() {
+		StringBuilder s = new StringBuilder();
+		s.append(intToLetter(c1));
+		s.append(r1);
+		s.append(intToLetter(c2));
+		s.append(r2);
+		return s.toString();
+	}
+	
+	public int letterToInt(char c) {
+		
+		switch(c) {
+		case 'a': return 0;
+		case 'b': return 1;
+		case 'c': return 2;
+		case 'd': return 3;
+		case 'e': return 4;
+		case 'f': return 5;
+		case 'g': return 6;
+		case 'h': return 7;
+		default: return -1;
+		}
+	}
+	
+	public int intToInt(char c) {
+		switch(c) {
+		case '1': return 0;
+		case '2': return 1;
+		case '3': return 2;
+		case '4': return 3;
+		case '5': return 4;
+		case '6': return 5;
+		case '7': return 6;
+		case '8': return 7;
+		default: return -1;
+		}
+	}
+	
+	public char intToLetter(int k) {
+		switch(k) {
+		case 0: return 'a';
+		case 1: return 'b';
+		case 2: return 'c';
+		case 3: return 'd';
+		case 4: return 'e';
+		case 5: return 'f';
+		case 6: return 'g';
+		case 7: return 'h';
+		default: return '!';
+		}
 	}
 }
---
+++ src/MoveTest.java
@@ -0,0 +1,56 @@
+import org.junit.Assert;
+import org.junit.Test;
+public class MoveTest {
+
+	//testing the move class
+	
+	//
+	@Test
+	public void testConstructor() {
+		
+		Move move = new Move("a1h8");
+		Assert.assertEquals(0, move.c1);
+		Assert.assertEquals(0, move.r1);
+		Assert.assertEquals(7, move.c2);
+		Assert.assertEquals(7, move.r2);
+		
+		move = new Move(1, 2, 8, 0);
+		Assert.assertEquals(2, move.c1);
+		Assert.assertEquals(1, move.r1);
+		Assert.assertEquals(0, move.c2);
+		Assert.assertEquals(8, move.r2);
+		
+	}
+	
+	public void testToString() {
+		Move move = new Move("a1h8");
+		Assert.assertEquals("a1h8", move.toString());
+		
+		move.c1 = 15; //weird case
+		Assert.assertEquals("!1h8", move.toString());
+	}
+	
+	
+	
+	//exceptions
+	@Test (expected=Exception.class)
+	public void testConstructorException() throws IllegalArgumentException{
+		new Move("");
+	}
+	@Test (expected=Exception.class)
+	public void testConstructorException1() throws IllegalArgumentException{
+		new Move("ab");
+	}
+	@Test (expected=Exception.class)
+	public void testConstructorException2() throws IllegalArgumentException{
+		new Move("abcd");
+	}
+	@Test (expected=Exception.class)
+	public void testConstructorException3() throws IllegalArgumentException{
+		new Move("a9b2");
+	}
+	@Test (expected=Exception.class)
+	public void testConstructorException4() throws IllegalArgumentException{
+		new Move("9ab2");
+	}
+}
